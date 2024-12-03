f7640e906a9c7c8341db7750b8873bd1dcf19738
phillip deleon <phillipdeleon68@gmail.com>
2024-12-03 13:41:27 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Assignment_5._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers to print: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            PrintNumber(input);
+            Console.WriteLine();
+
+
+           // Assignment_5._2._2
+            PrintNumberBackwards(input);
+
+          
+
+        }
+        static void PrintNumber (int number)
+        {
+
+            if (number > 0)
+            {
+                PrintNumber(number - 1);
+                Console.Write(number + " ");
+            }
+        }
+        //Assignment_5._2._3
+        static void PrintNumberBackwards(int number)
+        {
+            if (number > 0)
+            {
+                Console.Write(number + " ");
+                PrintNumberBackwards(number - 1);
+            }
+        }
+
+        
+    }
+}
